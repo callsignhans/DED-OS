821db8ba51c5f41f0c7195db8f0678a348c2b7be
callsignhans <genrihmuller812@gmail.com>
2026-09-17 14:51:25 +0700

    fix compile

--- boot.asm
+++ boot.asm
@@ -1,40 +1,40 @@
 [BITS 16]
 
 start:
-	cli
-	xor bx, bx
-	mov ss, bx
-	mov sp, 0x7C00
-	sti
+	cli                 ; Отключаем прерывания
+	xor bx, bx          ; Зануляем bx, запись для записи в регистр
+	mov ss, bx          ; Устанавливаем базу стека
+	mov sp, 0x7C00      ; Устанавливаем верхнюю границу стека на ячейку в которую выгружается загрузчик
+	sti                 ; Включаем прерывания
 
-	mov ax, 0x7C0
-	mov ds, ax
+	mov ax, 0x7C0       ;
+	mov ds, ax          ;
 
 driver_read:
-	push bx
-    pop cx
-    mov dh, 0
-    mov si, 0x1FE0
+	    push bx         ;
+        pop cx          ;
+        mov dh, 0       ;
+        mov si, 0x1FE0  ;
 .loop:
-    add si, 0x20
-    inc cl
-    cmp cl, 19
-    jnz .post
-    mov cl, 1
-    add ch, dh
-    xor dh, 1
+        add si, 0x20
+        inc cl
+        cmp cl, 19
+        jnz .post
+        mov cl, 1
+        add ch, dh
+        xor dh, 1
 .post:      
-    mov es, si
-    mov di, 4
+        mov es, si
+        mov di, 4
 .return:
-    mov ax, 0x0201
-    int 0x13
-    jc .error
-    cmp si, 0x7FE0
-    jnz .loop
-    jmp protected_mode_enable
+        mov ax, 0x0201
+        int 0x13
+        jc .error
+        cmp si, 0x7FE0
+        jnz .loop
+        jmp .end
 .error:
-        sub di, 1
+        sub di, 1   ;
         jnz .return
 .end:
         jmp .end
